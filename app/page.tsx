d56aef84fad961cee1d269df30752c45319cc097
Devolution <seanglyseanglay@gmail.com>
2024-01-10 21:46:51 +0700

    fix confict

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,13 @@
 import {Inter} from "@next/font/google";
 import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
-import ContactSection from "@/components/Contact";
 
 const inter = Inter({subsets: ["latin"]});
 
 export default function Home() {
-<<<<<<< HEAD
-  return (
-    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
-      <HeroSection />
-      <AboutSection />
-      <PassionSection />
-      {/* <ProjectsSection /> */}
-    </main>
-  );
-=======
+
     return (
         <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
             <HeroSection/>
         </main>
     );
->>>>>>> dev
 }
