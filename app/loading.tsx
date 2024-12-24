88eff2bcd44e3655a423c11c46e4e597a9339713
SEANGLAY <devolutionary.wizard@gmail.com>
2024-12-24 13:36:24 +0700

    clean up

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import {Loader} from "@/components/Loader";
-
-export default function Loading() {
-    return (
-        <div className="flex-center">
-            <Loader textClassNames="text-2xl text-center" />
-        </div>
-    );
-}
